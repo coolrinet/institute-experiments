4ecb819ae9e505629c0859683ad10196bcfb6b90
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2024-03-15 18:37:05 +0700

    Create resource routes for models

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\ExperimentController;
+use App\Http\Controllers\ExperimentParamController;
+use App\Http\Controllers\MachineryController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome');
+
+Route::resource('experiments', ExperimentController::class);
+
+Route::resource('machineries', MachineryController::class)->except('show');
+
+Route::resource('experiment_params', ExperimentParamController::class)->except('show');
