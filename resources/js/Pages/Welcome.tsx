b605aa1cfba953552c8ac9ed2a825f66ba91b4f1
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2024-03-17 14:35:01 +0700

    Install Tailwind CSS

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,11 +1,12 @@
 import { Head } from '@inertiajs/react';
-import styles from './Welcome.module.css';
 
 export default function Welcome() {
   return (
     <>
       <Head title='Добро пожаловать' />
-      <h1 className={styles.heading}>Привет, мир!</h1>
+      <h1 className='select-none text-center text-4xl font-bold italic transition-colors hover:text-red-600'>
+        Привет, мир!
+      </h1>
     </>
   );
 }
